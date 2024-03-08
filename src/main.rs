c18d0aba1bfb6fe2768d709b6a418ae145605c21
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2024-03-08 19:12:47 +0100

    Return error if configuration is not present

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,27 @@ fn main()
 {
     let output = Command::new("sh")
         .arg("-c")
-        .arg(tcr(config))
+        .arg(tcr(config).expect(""))
         .output()
         .expect("failed to execute process");
     io::stdout().write_all(&output.stdout).unwrap()
 }
 
-fn config() -> String
+fn config() -> Option<String>
 {
-    String::from("cargo test")
+    Some(String::from("cargo test"))
 }
 
 #[allow(unused)]
-fn tcr(config: fn() -> String) -> String
+fn tcr(config: fn() -> Option<String>) -> Result<String, String>
 {
-    return format!("{} && git add . && git commit -m WIP || git reset --hard", config());
+    if (config().is_none())
+    {
+        return Err(String::from(""))
+    }
+    return Ok(format!(
+        "{} && git add . && git commit -m WIP || git reset --hard",
+        config().expect("")));
 }
 
 #[cfg(test)]
@@ -34,10 +40,20 @@ mod tests
     #[test]
     fn it_runs_tcr()
     {
-        fn test_conf() -> String
+        fn test_conf() -> Option<String>
         {
-            return String::from("cargo test")
+            return Some(String::from("cargo test"))
         }
-        assert_eq!(tcr(test_conf), "cargo test && git add . && git commit -m WIP || git reset --hard");
+        assert_eq!(tcr(test_conf).expect(""), "cargo test && git add . && git commit -m WIP || git reset --hard");
+    }
+
+    #[test]
+    fn it_returns_error_if_configuration_is_not_present()
+    {
+        fn no_conf() -> Option<String>
+        {
+            return None
+        }
+        assert!(tcr(no_conf).is_err());
     }
 }
