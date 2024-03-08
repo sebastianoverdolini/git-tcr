808a071a536af3ffc4f48c84d33913de2e6b1b49
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2024-03-08 18:15:48 +0100

    Put curly braces on new lines

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
-fn main() {
+fn main()
+{
     println!("Hello, world!");
 }
 
 #[cfg(test)]
-mod tests {
+mod tests
+{
     #[test]
-    fn it_works() {
+    fn it_works()
+    {
         let result = 2 + 2;
         assert_eq!(result, 4);
     }
