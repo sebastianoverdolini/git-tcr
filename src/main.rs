572fb97aad26225447d8e193f3359aedb64b486a
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2024-03-08 18:48:45 +0100

    Test 'Hello world' returning from tcr

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,19 @@ fn main()
     println!("Hello, world!");
 }
 
+fn tcr() -> &'static str
+{
+    return "Hello, world!"
+}
+
 #[cfg(test)]
 mod tests
 {
+    use crate::tcr;
+
     #[test]
     fn it_works()
     {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        assert_eq!(tcr(), "Hello, world!");
     }
 }
