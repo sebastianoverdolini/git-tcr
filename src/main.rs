8ee25bbea1112c827be38dda49290c7a73c39a82
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2024-03-08 18:52:00 +0100

    Call cal from tcr and run the cmd in main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
+extern crate core;
+
+use std::io;
+use std::io::Write;
+use std::process::Command;
+
 fn main()
 {
-    println!("{}", tcr());
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg(tcr())
+        .output()
+        .expect("failed to execute process");
+    io::stdout().write_all(&output.stdout).unwrap()
 }
 
 fn tcr() -> &'static str
 {
-    return "Hello, world!"
+    return "cal"
 }
 
 #[cfg(test)]
@@ -16,6 +27,6 @@ mod tests
     #[test]
     fn it_works()
     {
-        assert_eq!(tcr(), "Hello, world!");
+        assert_eq!(tcr(), "cal");
     }
 }
