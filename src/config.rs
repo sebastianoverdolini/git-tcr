4c35dc86f7adafbce748594c31f4764bb0fa02eb
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2025-04-04 16:57:59 +0200

    Make config tests deterministic

--- src/config.rs
+++ src/config.rs
@@ -23,17 +23,20 @@ pub fn yaml_config(location: impl Into<PathBuf>) -> Option<Config>
 }
 
 #[cfg(test)]
-mod yaml_config_tests
-{
+mod yaml_config_tests {
     use std::fs::{create_dir_all, remove_dir_all, write};
     use std::path::Path;
     use crate::config;
     use crate::config::Config;
 
     #[test]
-    fn it_returns_the_content_of_the_config_if_the_file_is_present_in_the_current_location()
-    {
-        create_dir_all("test-env").expect("TODO: panic message");
+    fn it_returns_the_content_of_the_config_if_the_file_is_present_in_the_current_location() {
+        let test_dir = "test-env-content-check";
+        let config_path = format!("{}/tcr.yaml", test_dir);
+
+        let _ = remove_dir_all(test_dir);
+        create_dir_all(test_dir).expect("Failed to create test directory");
+
         let yaml_string = r#"
         test: npm test
         before:
@@ -41,13 +44,12 @@ mod yaml_config_tests
             - prettier --write .
         no_verify: true
         "#;
-        write("test-env/tcr.yaml", yaml_string).expect("TODO: panic message");
+        write(&config_path, yaml_string).expect("Failed to write test config");
 
-        let result = config::yaml_config(Path::new("./test-env"));
+        let result = config::yaml_config(Path::new(test_dir));
 
         assert!(result.is_some());
-        assert_eq!(result.unwrap(), Config
-        {
+        assert_eq!(result.unwrap(), Config {
             test: String::from("npm test"),
             before: vec![
                 String::from("pnpm tc"),
@@ -56,34 +58,41 @@ mod yaml_config_tests
             no_verify: true
         });
 
-        remove_dir_all("test-env").expect("TODO: panic message");
+        remove_dir_all(test_dir).expect("Failed to remove test directory");
     }
 
     #[test]
-    fn no_verify_option_is_false_by_default()
-    {
-        create_dir_all("test-env-2").expect("TODO: panic message");
-        write("test-env-2/tcr.yaml", r#"
+    fn no_verify_option_is_false_by_default() {
+        let test_dir = "test-env-no-verify";
+        let config_path = format!("{}/tcr.yaml", test_dir);
+
+        let _ = remove_dir_all(test_dir);
+        create_dir_all(test_dir).expect("Failed to create test directory");
+
+        write(&config_path, r#"
         test: npm test
         before: []
-        "#).expect("TODO: panic message");
+        "#).expect("Failed to write test config");
 
-        let result = config::yaml_config(Path::new("./test-env-2"));
+        let result = config::yaml_config(Path::new(test_dir));
 
         assert!(result.is_some());
-        assert_eq!(result.unwrap(), Config
-        {
+        assert_eq!(result.unwrap(), Config {
             test: String::from("npm test"),
             before: vec![],
             no_verify: false
         });
 
-        remove_dir_all("test-env-2").expect("TODO: panic message");
+        remove_dir_all(test_dir).expect("Failed to remove test directory");
     }
 
     #[test]
-    fn it_returns_none_if_the_config_file_is_not_present_in_the_current_location()
-    {
-        assert!(config::yaml_config(String::from("./test-env")).is_none());
+    fn it_returns_none_if_the_config_file_is_not_present_in_the_current_location() {
+        let test_dir = "test-env-none-check";
+
+        let _ = remove_dir_all(test_dir);
+
+        assert!(config::yaml_config(Path::new(test_dir)).is_none());
     }
 }
+
