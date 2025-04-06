0f31177f587c156e65f36c25f54178b3d5a3593e
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2025-04-06 14:46:48 +0200

    Integrate --watch

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,96 @@
 extern crate core;
 
 use std::env::current_dir;
-use std::process::Command;
+use std::path::{Path};
+use std::process::{Command, Stdio};
+use std::time::Duration;
+use clap::Parser;
+use notify::{RecursiveMode};
+use notify_debouncer_full::new_debouncer;
 use crate::config::yaml_config;
-use crate::tcr::{tcr_cmd, TcrCommand};
+use crate::tcr::{tcr_cmd};
 
 mod tcr;
 mod config;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(long)]
+    watch: bool,
+}
+
 fn main()
 {
+    let args = Args::parse();
+
+    if args.watch
+    {
+        watched(tcr)
+    }
+    else
+    {
+        tcr()
+    }
+}
+
+fn tcr() {
     let result = tcr_cmd(|| yaml_config(current_dir().unwrap()));
     match result {
-        Ok(cmd) => sh(cmd),
+        Ok(cmd) => {
+            Command::new("sh")
+                .arg("-c")
+                .arg(cmd)
+                .spawn()
+                .expect("failed to execute process")
+                .wait()
+                .expect("TODO: panic message");
+        }
         Err(error) => println!("Error: {}", error)
     }
 }
 
-fn sh(cmd: TcrCommand)
-{
-    Command::new("sh")
-        .arg("-c")
-        .arg(cmd)
-        .spawn()
-        .expect("failed to execute process")
-        .wait()
-        .expect("TODO: panic message");
+fn watched(tcr: fn()) {
+    let (tx, rx) = std::sync::mpsc::channel();
+
+    let mut debouncer = new_debouncer(Duration::from_secs(1), None, tx).unwrap();
+    debouncer
+        .watch(Path::new("."), RecursiveMode::Recursive)
+        .unwrap();
+
+    for result in rx {
+        match result {
+            Ok(events) => {
+                let is_considered = events.iter()
+                    .any(|e| e.paths.iter().any(|path|
+                        !is_within_git_directory(path) &&
+                            !is_git_ignored(path)
+                    ));
+                if is_considered {
+                    tcr();
+                }
+            },
+            Err(error) => println!("Error {error:?}"),
+        }
+    }
+}
+
+fn is_within_git_directory(path: &Path) -> bool {
+    let path_str = path.to_str().unwrap();
+    path_str.contains("/.git/") || path_str == ".git"
+}
+
+fn is_git_ignored(path: &Path) -> bool {
+    let path_str = path.to_str().unwrap();
+
+    let output = Command::new("git")
+        .arg("check-ignore")
+        .arg(path_str)
+        .stderr(Stdio::null())
+        .output();
+
+    match output {
+        Ok(output) => !output.stdout.is_empty(),
+        Err(_) => false,
+    }
 }
