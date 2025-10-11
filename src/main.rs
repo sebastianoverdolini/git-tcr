8f3c1cd1bc41382a40e48c8db2d0e46576f9af38
Sebastiano Verdolini <sebastiano.verdolini@gmail.com>
2025-10-11 12:02:58 +0200

    Install clap and use it to retrieve --scribe option

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,35 @@
 extern crate core;
 
-use std::env::{self, current_dir};
+use std::env::{current_dir};
 use std::process::Command;
 use crate::config::yaml_config;
 use crate::message::{scribe, wip};
 use crate::tcr::tcr;
+use clap::Parser;
 
 mod tcr;
 mod config;
 mod git;
 mod message;
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[arg(long)]
+    scribe: bool,
+}
+
 fn main()
 {
+    let cli = Cli::parse();
     match yaml_config(current_dir().unwrap()) {
         Some(configuration) => {
             let git = git::GitRepository {
                 config: configuration.clone(),
                 exec: Box::new(|cmd: &mut Command| {
-                    cmd
-                        .output()
+                    cmd.output()
                 }),
-                message: if env::args().any(|arg| arg == "--scribe") { scribe } else { wip },
+                message: if cli.scribe { scribe } else { wip },
             };
             tcr(&git)
         },
